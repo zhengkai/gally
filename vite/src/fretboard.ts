e49826a6d723eb6720e72a4e0470fb3dbc45254c
Zheng Kai <zhengkai@gmail.com>
2025-07-09 10:18:12 +0800

    stage1

--- vite/src/fretboard.ts
+++ vite/src/fretboard.ts
@@ -1,13 +1,34 @@
 class Fretboard {
 
-	run() {
-		console.log("Fretboard is running");
+	config = {
 
+	}
+
+	tuning = [40, 45, 50, 55, 59, 64];
+	noteNames = ['C', 'C♯', 'D', 'D♯', 'E', 'F', 'F♯', 'G', 'G♯', 'A', 'A♯', 'B'];
+
+	run() {
 		document.querySelector<HTMLDivElement>('#fretboard')!.innerHTML = this.genHTML();
 	}
 
+	getSymbol(stringID: number, fretID: number): string {
+		const symbol = this.noteNames[(this.tuning[stringID] + fretID) % 12];
+		const level = Math.floor((this.tuning[stringID] + fretID) / 12);
+		return `${symbol}${level}`;
+	}
+
 	genHTML() {
-		return 'Fretboard is running';
+
+		let s = '';
+		[...Array(6).keys()].forEach(stringID => {
+			s += `<div class="string" id="string-${stringID}">`;
+			[...Array(24).keys()].forEach(fertID => {
+				s += `<div class="fret" id="fret-${stringID}-${fertID}"><div class="symbol">${this.getSymbol(stringID, fertID)}</div></div>`;
+			})
+			s += '</div>';
+		})
+
+		return s;
 	}
 }
 
